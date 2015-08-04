354709c57f66bf4fce12cc90e2c3a092322f25a8
SnowyEgret <duncan.m.swain@gmail.com>
2015-08-03 20:48:15 -0400

    Misc

--- src/main/java/org/snowyegret/geom/matrix/RotationMatrix.java
+++ src/main/java/org/snowyegret/geom/matrix/RotationMatrix.java
@@ -2,6 +2,7 @@ package org.snowyegret.geom.matrix;
 
 import javax.vecmath.AxisAngle4d;
 import javax.vecmath.Matrix4d;
+import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
 import org.snowyegret.geom.GeomUtil;
@@ -13,8 +14,10 @@ public class RotationMatrix extends Matrix4d {
 		setIdentity();
 		double angle = v1.angle(v2);
 		Vector3d axis = new Vector3d();
-		if (Math.abs(angle - Math.PI) < .0000001 || Math.abs(angle - 0) < .0000001) {
-			//System.out.println("[RotationMatrix] Vectors are parallel. angle=" + angle);
+		if (Math.abs(angle - Math.PI) < .0000001
+				|| Math.abs(angle - 0) < .0000001) {
+			// System.out.println("[RotationMatrix] Vectors are parallel. angle="
+			// + angle);
 			axis = GeomUtil.anyOrthogonalVector(v1);
 		} else {
 			axis.cross(v1, v2);
@@ -23,9 +26,23 @@ public class RotationMatrix extends Matrix4d {
 		// System.out.println(axis);
 		setRotation(new AxisAngle4d(axis, angle));
 	}
-	
-	public RotationMatrix(Vector3d axis, double angle) {
+
+	// Rotates around a point center
+	public RotationMatrix(Point3d center, Vector3d axis, double angle) {
+
+		Matrix4d mR = new Matrix4d();
+		mR.setIdentity();
 		axis.normalize();
-		setRotation(new AxisAngle4d(axis, angle));
+		mR.setRotation(new AxisAngle4d(axis, angle));
+
+		Matrix4d mT = new Matrix4d();
+		mT.setIdentity();
+		mT.setTranslation(new Vector3d(center));
+
+		setIdentity();
+		mul(mT);
+		mul(mR);
+		mT.invert();
+		mul(mT);
 	}
 }
--- src/main/java/org/snowyegret/geom/surface/SeaShell.java
+++ src/main/java/org/snowyegret/geom/surface/SeaShell.java
@@ -12,11 +12,28 @@ public class SeaShell extends Surface {
 	// double n = 2.0;
 
 	double k = 40;
+	double a = k / 5;
+	double b = a / 2;
 
-	public SeaShell(Point3d origin) {
+	// double c = 3 / 4 * a;
+
+	// Must be an origin, other arguments can be null
+	public SeaShell(Point3d origin, Point3d numTurnsIn) {
 		super(origin);
-		rU = Range.between(0d, 4 * Math.PI);
-		rV = Range.between(0d, 4 * Math.PI);
+//		rU = Range.between(0d, 4 * Math.PI);
+//		rV = Range.between(0d, 4 * Math.PI);
+		double numTurns = Math.abs(origin.distance(numTurnsIn));
+		rU = Range.between(0d, numTurns * Math.PI);
+		rV = Range.between(0d, numTurns * Math.PI);
+		k = 30;
+//		a = Math.abs(numTurns.distance(aIn)) * Math.PI * 2;
+//		b = Math.abs(aIn.distance(bIn)) * Math.PI * 2;
+		a = 11 * Math.PI;
+		b = 5 * Math.PI;
+		// b = a / 2d * Math.PI;
+		// c = 3d * a / 4d;
+
+		System.out.println(this);
 	}
 
 	// //http://mathworld.wolfram.com/Seashell.html
@@ -25,7 +42,8 @@ public class SeaShell extends Surface {
 	// Point3d p = new Point3d();
 	// p.x = ((1 - v / 2 * Math.PI) * (1 + Math.cos(u)) + c) * Math.cos(n * v);
 	// p.y = ((1 - v / 2 * Math.PI) * (1 + Math.cos(u)) + c) * Math.cos(n * v);
-	// p.z = (b * v) / (2 * Math.PI) + a * Math.sin(u) * (1 - v / (2 * Math.PI));
+	// p.z = (b * v) / (2 * Math.PI) + a * Math.sin(u) * (1 - v / (2 *
+	// Math.PI));
 	// p.add(p0);
 	// return p;
 	// }
@@ -33,19 +51,46 @@ public class SeaShell extends Surface {
 	// http://mathworld.wolfram.com/Seashell.html
 	@Override
 	public Point3d pointAtParameters(double u, double v) {
+		double cosVoverTwoSquared = Math.cos(v / 2) * Math.cos(v / 2);
+		double eToTheUoverA = Math.pow(Math.E, u / a);
+		double sinV = Math.sin(v);
 		Point3d p = new Point3d();
-		p.z = k * (2 * (1 - Math.pow(Math.E, u / (8 * Math.PI))) * Math.cos(u) * Math.cos(v / 2) * Math.cos(v / 2));
-		p.x = k * (2 * (-1 + Math.pow(Math.E, u / (8 * Math.PI))) * Math.sin(u) * Math.cos(v / 2) * Math.cos(v / 2));
-		p.y = -k
-				* (1 - Math.pow(Math.E, u / (4 * Math.PI)) - Math.sin(v) + Math.pow(Math.E, u / (6 * Math.PI))
-						* Math.sin(v));
+		p.z = k * (2 * (1 - eToTheUoverA) * Math.cos(u) * cosVoverTwoSquared);
+		p.x = k * (2 * (-1 + eToTheUoverA) * Math.sin(u) * cosVoverTwoSquared);
+		p.y = -k * (1 - Math.pow(Math.E, u / b) - sinV + eToTheUoverA * sinV);
+
+		// p.z = k
+		// * (2 * (1 - Math.pow(Math.E, u / a)) * Math.cos(u)
+		// * Math.cos(v / 2) * Math.cos(v / 2));
+		// p.x = k
+		// * (2 * (-1 + Math.pow(Math.E, u / a)) * Math.sin(u)
+		// * Math.cos(v / 2) * Math.cos(v / 2));
+		// p.y = -k
+		// * (1 - Math.pow(Math.E, u / b) - Math.sin(v) + Math.pow(Math.E,
+		// u / a) * Math.sin(v));
 		p.add(p0);
 		return p;
 	}
 
 	@Override
 	public boolean contains(Point3d p) {
-		throw new UnsupportedOperationException("Method 'Surface.contains' not yet implemented");
+		throw new UnsupportedOperationException(
+				"Method 'Surface.contains' not yet implemented");
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("SeaShell [size=");
+		builder.append(k);
+		builder.append(", a=");
+		builder.append(a);
+		builder.append(", b=");
+		builder.append(b);
+		// builder.append(", c=");
+		// builder.append(c);
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
